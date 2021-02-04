7a5584decf1c61d098a6f6959275a126c3f87a8a
Junxiong <junxiong@dhcp-vl2041-3084.eduroam.cornell.edu>
2021-02-04 15:33:36 -0500

    clean

--- pytpcc/gym-olapgame/gym_olapgame/__init__.py
+++
@@ -1,6 +0,0 @@
-from gym.envs.registration import register
-
-register(
-    id='olapgame-v0',
-    entry_point='gym_olapgame.envs:OLAPOptimizationGameEnv',
-)
--- pytpcc/mcts/__init__.py
+++
@@ -1,3 +0,0 @@
-# -*- coding: utf-8 -*-
-
-__all__ = ["delay_exp3_node", "delay_uct_node", "uct_node"]
