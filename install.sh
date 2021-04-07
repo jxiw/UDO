9eb7e0cae58122b1c9eb31fc852efd89c8046524
Junxiong <junxiong@dhcp-vl2041-3204.eduroam.cornell.edu>
2021-04-07 15:53:52 -0400

    add installation scrpits

---
+++ install.sh
@@ -0,0 +1,7 @@
+sudo apt -y update
+sudo apt -y install mysql-server
+sudo apt -y install postgresql postgresql-contrib
+sudo apt-get -y install python3-pip
+sudo apt-get -y install python3-dev libmysqlclient-dev
+sudo apt -y install libpq-dev
+
